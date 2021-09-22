7f314ce4aba32df423cf52b0299399f64afdef74
YehiaAyad <48591761+YehiaAyad@users.noreply.github.com>
2021-09-22 16:07:20 +0200

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -32,7 +32,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes),BrowserAnimationsModule],
+  imports: [RouterModule.forRoot(routes, {useHash: true}),BrowserAnimationsModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
